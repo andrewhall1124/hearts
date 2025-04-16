5bcfe5cbac41a0b9982c95506a6f743ca8f5cb96
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-04-16 12:46:19 -0600

    Ran analysis.

--- experiments/analysis.py
+++ experiments/analysis.py
@@ -10,10 +10,15 @@ df = pl.read_parquet(f"results/{file_name}.parquet")
 
 print(df)
 
+df = (
+    df.with_columns(pl.col('score').rank('ordinal').over('game_name').alias('rank'))
+    .sort('game_name')
+)
+
 n_games = df.select("game_name").unique().count()["game_name"].last()
 
 df = df.group_by("player").agg(
-    pl.col("score").mean().alias("mean"), pl.col("score").std().alias("std")
+    pl.col("score").mean().alias("mean"), pl.col("score").std().alias("std"), pl.col('rank').mean().alias('rank')
 )
 
 order = {"Random": 0, "MinCard": 1, "MinMaxCard": 2, "Sluffing": 3, "MCTS": 4}
@@ -35,7 +40,7 @@ plt.figure(figsize=(10, 6))
 
 sns.barplot(df, x="player", y="mean", color='red')
 
-plt.title(f"{n_games} Trials Results: Mean")
+plt.title(f"{n_games} Trials Results: Point Mean")
 
 plt.xlabel("Agent")
 plt.ylabel("Average cards won")
@@ -47,9 +52,21 @@ plt.figure(figsize=(10, 6))
 
 sns.barplot(df, x="player", y="std", color='blue')
 
-plt.title(f"{n_games} Trials Results: Standard Deviation")
+plt.title(f"{n_games} Trials Results: Point Standard Deviation")
 
 plt.xlabel("Agent")
 plt.ylabel("Standard deviation of cards won")
 
 plt.savefig(f"charts/{file_name}_std.png", dpi=300)
+plt.clf()
+
+plt.figure(figsize=(10, 6))
+
+sns.barplot(df, x="player", y="rank", color='green')
+
+plt.title(f"{n_games} Trials Results: Rank Mean")
+
+plt.xlabel("Agent")
+plt.ylabel("Mean rank of games")
+
+plt.savefig(f"charts/{file_name}_rank.png", dpi=300)
